16d1a0f7d0023298cd8506f7f546f34ed491fe0b
Fahim <fracturx@outlook.com>
2020-05-20 18:09:14 -0400

    Updated List of Apis and added endpoints

--- src/models/Auth.ts
+++ src/models/Auth.ts
@@ -1,14 +1,47 @@
-export const endpoints = {
+export const apis = {
   key_value: ".ROBLOSECURITY=",
-  Users: "https://users.roblox.com/v1/users/",
+  Abuse: "https://abuse.roblox.com/v1/",
+  AccountSettings: "https://accountsettings.roblox.com/docs",
+  Api: "https://api.roblox.com/",
+  Badges: "https://badges.roblox.com/v1",
+  Billing: "https://billing.roblox.com/v1/",
+  Catalog: "https://catalog.roblox.com/v1",
+  Develop: "https://develop.roblox.com/v1",
+  Followings: "https://followings.roblox.com/v1",
+  Friends: "https://friends.roblox.com/v1/",
+  Users: "https://users.roblox.com/v1/",
   Economy: "https://economy.roblox.com/v1",
   AssetDelivery : "https://assetdelivery.roblox.com/v1",
+  Inventory: "https://inventory.roblox.com/",
+  Locale: "https://locale.roblox.com/v1",
+  Notifications: "https://notifications.roblox.com/v2",
   Party : "https://party.roblox.com/v1",
   Presence : "https://presence.roblox.com/v1",
-  Group : "https://group.roblox.com/v1/",
+  Publish: "https://publish.roblox.com/v1/",
+  Thumbnails: "https://thumbnails.roblox.com/v1",
+  Group : "https://groups.roblox.com/v1/",
   Games : "https://games.roblox.com/v1/",
   Chat: "https://chat.roblox.com/v2/",
   Avatar: "https://avatar.roblox.com/v1/",
+}
+
+export const endpoints = {
+  v1: "/v1",
+  v2: "/v2",
+  users: "/users",
+  games: "/games",
+  avatar: "/avatar",
+  outfits: "/outfits",
+  badges: "/badges",
+  assets: "/assets",
+  notifications: "/notificiations",
+  push_notifications: "/push-notifications",
+  stream_notifications: "/stream-notifications",
+  friends: "/friends",
+  metadata: "/metadata",
+  my: "/my",
+  user: "/user",
+  
 }
 
 export const headers = {
